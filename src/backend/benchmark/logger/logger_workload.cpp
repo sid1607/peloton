7054f5782a1f14d2754b513087bb2b358ffe71a3
user <jarulraj@cs.cmu.edu>
2016-08-09 00:53:43 -0700

    Added wait timeout in output

--- src/backend/benchmark/logger/logger_workload.cpp
+++ src/backend/benchmark/logger/logger_workload.cpp
@@ -72,11 +72,11 @@ size_t GetLogFileSize();
 
 static void WriteOutput(double value) {
   LOG_INFO("----------------------------------------------------------");
-  LOG_INFO("%d %d %lf %d %d %d %d %d %d %d :: %lf", state.benchmark_type,
+  LOG_INFO("%d %d %lf %d %d %d %d %d %d %d %d :: %lf", state.benchmark_type,
            state.logging_type, ycsb::state.update_ratio,
            ycsb::state.backend_count, ycsb::state.scale_factor,
            ycsb::state.duration, state.nvm_latency, state.pcommit_latency,
-           state.flush_mode, state.asynchronous_mode, value);
+           state.flush_mode, state.asynchronous_mode, state.wait_timeout, value);
 
   out << state.benchmark_type << " ";
   out << state.logging_type << " ";
@@ -88,6 +88,7 @@ static void WriteOutput(double value) {
   out << state.pcommit_latency << " ";
   out << state.flush_mode << " ";
   out << state.asynchronous_mode << " ";
+  out << state.wait_timeout << " ";
   out << value << "\n";
   out.flush();
 }
